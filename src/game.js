d9f88019eaa0ebad9ef7918b14887fa8dd8be3e2
Sasha Vatamanyuk <alexandervatamanyuk@gmail.com>
2020-02-22 18:13:13 +0200

    code refactoring

--- src/game.js
+++ src/game.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export default (description, makeDataGame, roundsCount = 3) => {
+const roundsCount = 3;
+
+export default (description, makeDataGame) => {
   console.log('Welcome to the Brain Games!');
   console.log(description);
   const userName = readlineSync.question('May I have your name? ');
